b3a2795777bc7690c0816c683cc2ed45ac6afb14
ngf93 <nabiullinaguzel93@gmail.com>
2023-04-19 10:09:35 +0300

    desktop

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
